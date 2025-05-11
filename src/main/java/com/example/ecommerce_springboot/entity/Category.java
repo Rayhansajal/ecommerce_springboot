15752688c5f808a42c9ec889821b6c6c0717b65b
Rayhansajal <rayhansajal1996@gmail.com>
2025-05-11 11:49:28 +0600

    second commit

--- src/main/java/com/example/ecommerce_springboot/entity/Category.java
+++ src/main/java/com/example/ecommerce_springboot/entity/Category.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce_springboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Data
@@ -14,5 +16,6 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+    @NotBlank(message = "can not be blank")
     private String categoryName;
 }
